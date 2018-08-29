9947d519e2dd06c0ce3d61afa6018550408eeac9
PanktiParikh <panktip85@gmail.com>
2018-08-29 13:28:29 -0400

    students model defined

--- db/models/student.js
+++ db/models/student.js
@@ -3,6 +3,36 @@
 const Sequelize = require('sequelize');
 const db = require('../db');
 
-const Student = db.define();
+const Student = db.define('student', {
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    // validate: {
+    //   is: /[\w]+/,
+    // },
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    // validate: {
+    //   is: /[\w]+/,
+    // },
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
+
+Student.beforeCreate(student => {
+  const nameFirst = student.firstName;
+  const nameLast = student.lastName;
+
+  student.firstName = nameFirst[0].toUpperCase() + nameFirst.slice(1);
+  student.lastName = nameLast[0].toUpperCase() + nameLast.slice(1);
+});
 
 module.exports = Student;
