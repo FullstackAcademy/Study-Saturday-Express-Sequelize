f06735a66800392c3087592a07b2c7c59246d0c7
Blake Ferm <blakeferm@gmail.com>
2020-05-02 16:21:35 -0400

    update repo

--- routes/students.js
+++ routes/students.js
@@ -1,57 +1,62 @@
+// require the router 
 const router = require('express').Router();
 const Student = require('../db/models/student');
 
-router.get('/', async (req, res, next) => {
-  try {
-    const students = await Student.findAll();
-    res.send(students);
-  } catch (error) {
-    next(error);
-  }
-});
-
-router.get('/:id', async (req, res, next) => {
-  try {
-    let student = await Student.findById(req.params.id);
-    if (student) {
-      res.send(student);
-    } else {
-      res.status(404).send('Student not found');
+// setup routes
+router.get('/', async(req, res, next) => {
+    try {
+        const students = await Student.findAll();
+        res.send(students);
+    } catch (error) {
+        next(error);
     }
-  } catch (error) {
-    next(error);
-  }
 });
 
-router.post('/', async (req, res, next) => {
-  try {
-    let student = await Student.create(req.body);
-    res.status(201).send(student);
-  } catch (err) {
-    next(err);
-  }
+router.get('/:id', async(req, res, next) => {
+    try {
+        let student = await Student.findById(req.params.id);
+        if (student) {
+            res.send(student);
+        } else {
+            res.status(404).send('Student not found');
+        }
+    } catch (error) {
+        next(error);
+    }
 });
 
-router.put('/:id', async (req, res, next) => {
-  try {
-    let updatedStudentInfo = await Student.update(req.body, {
-      where: { id: req.params.id },
-      returning: true,
-      plain: true,
-    });
-    res.send(updatedStudentInfo[1]);
-  } catch (err) {
-    next(err);
-  }
+router.post('/', async(req, res, next) => {
+    try {
+        let student = await Student.create(req.body);
+        res.status(201).send(student);
+    } catch (err) {
+        next(err);
+    }
 });
 
-router.delete('/:id', async (req, res, next) => {
-  try {
-    await Student.destroy({ where: { id: req.params.id } });
-    res.status(204).send();
-  } catch (err) {
-    next(err);
-  }
+router.put('/:id', async(req, res, next) => {
+    let updatedStudent = req.params.id;
+    try {
+        let updatedStudentInfo = await Student.update(req.body, {
+            where: { id: updatedStudent },
+            returning: true,
+            plain: true,
+        });
+        res.send(updatedStudentInfo[1]);
+    } catch (err) {
+        next(err);
+    }
 });
 
+router.delete('/:id', async(req, res, next) => {
+    let deletedStudent = req.params.id;
+    try {
+        await Student.destroy({ where: { id: deletedStudent } });
+        res.status(204).send();
+    } catch (err) {
+        next(err);
+    }
+});
+
+// export the router
 module.exports = router;
