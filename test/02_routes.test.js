aed2c3526b829759a62a20985ab600aa5bc9f9b6
Mary Gordanier <mcgordanier@gmail.com>
2020-11-15 16:26:32 -0500

    refactor: change .findById to .findByPk everywhere

--- test/02_routes.test.js
+++ test/02_routes.test.js
@@ -1,30 +1,30 @@
-'use strict';
-const expect = require('chai').expect;
-const request = require('supertest');
+'use strict'
+const expect = require('chai').expect
+const request = require('supertest')
 
-const app = require('../app');
-const agent = request.agent(app);
+const app = require('../app')
+const agent = request.agent(app)
 
-const db = require('../db/db');
-const Student = require('../db/models/student');
-const Test = require('../db/models/test');
+const db = require('../db/db')
+const Student = require('../db/models/student')
+const Test = require('../db/models/test')
 
 describe('Routes', () => {
   before(() => {
-    return db.sync({ force: true });
-  });
+    return db.sync({ force: true })
+  })
 
   afterEach(() => {
     return Promise.all([
       Student.truncate({ cascade: true }),
       Test.truncate({ cascade: true }),
-    ]);
-  });
+    ])
+  })
 
   describe('Student Routes', () => {
-    let pepper;
-    let peter;
-    let charlie;
+    let pepper
+    let peter
+    let charlie
 
     beforeEach(() => {
       const creatingStudents = [
@@ -43,13 +43,13 @@ describe('Routes', () => {
           lastName: 'Brown',
           email: 'cb@cbdb.com',
         },
-      ].map(data => Student.create(data));
-      return Promise.all(creatingStudents).then(createdStudents => {
-        pepper = createdStudents[0];
-        peter = createdStudents[1];
-        charlie = createdStudents[2];
-      });
-    });
+      ].map((data) => Student.create(data))
+      return Promise.all(creatingStudents).then((createdStudents) => {
+        pepper = createdStudents[0]
+        peter = createdStudents[1]
+        charlie = createdStudents[2]
+      })
+    })
 
     describe('GET /students', () => {
       it('retrieves all the students', () => {
@@ -57,28 +57,28 @@ describe('Routes', () => {
           .get('/students')
           .expect('Content-Type', /json/)
           .expect(200)
-          .expect(res => {
-            expect(res.body).to.be.an.instanceOf(Array);
-            expect(res.body).to.have.length(3);
-          });
-      });
-    });
+          .expect((res) => {
+            expect(res.body).to.be.an.instanceOf(Array)
+            expect(res.body).to.have.length(3)
+          })
+      })
+    })
 
     describe('GET /students/:id', () => {
       it('retrieves a single student by their id', () => {
         return agent
           .get(`/students/${pepper.id}`)
           .expect(200)
-          .expect(res => {
-            if (typeof res.body === 'string') res.body = JSON.parse(res.body);
-            expect(res.body.firstName).to.equal('Pepper');
-          });
-      });
+          .expect((res) => {
+            if (typeof res.body === 'string') res.body = JSON.parse(res.body)
+            expect(res.body.firstName).to.equal('Pepper')
+          })
+      })
 
       it('returns a 404 error if student does not exist in DB', () => {
-        return agent.get('/students/09432').expect(404);
-      });
-    });
+        return agent.get('/students/09432').expect(404)
+      })
+    })
 
     describe('POST /students', () => {
       it('creates a new Student instance', () => {
@@ -91,11 +91,11 @@ describe('Routes', () => {
           })
           .expect(201)
           .expect('Content-Type', /json/)
-          .expect(res => {
-            expect(res.body.firstName).to.equal('SQL');
-          });
-      });
-    });
+          .expect((res) => {
+            expect(res.body.firstName).to.equal('SQL')
+          })
+      })
+    })
 
     describe('PUT /students/:id', () => {
       it('updates an instance of a student', () => {
@@ -104,11 +104,11 @@ describe('Routes', () => {
           .send({ firstName: 'Salty' })
           .expect(200)
           .expect('Content-Type', /json/)
-          .expect(res => {
-            expect(res.body.firstName).to.equal('Salty');
-          });
-      });
-    });
+          .expect((res) => {
+            expect(res.body.firstName).to.equal('Salty')
+          })
+      })
+    })
 
     describe('DELETE /students/:id', () => {
       it('deletes an instance of a student', () => {
@@ -116,19 +116,19 @@ describe('Routes', () => {
           .delete(`/students/${charlie.id}`)
           .expect(204)
           .expect(() => {
-            return Student.findById(charlie.id).then(res =>
+            return Student.findByPk(charlie.id).then((res) =>
               expect(res).to.equal(null)
-            );
-          });
-      });
-    });
-  });
+            )
+          })
+      })
+    })
+  })
 
   describe('Test Routes', () => {
-    let funTest;
-    let badTest;
-    let hardTest;
-    let crayTest;
+    let funTest
+    let badTest
+    let hardTest
+    let crayTest
     beforeEach(() => {
       const creatingTests = [
         {
@@ -147,55 +147,55 @@ describe('Routes', () => {
           subject: 'Outdoor Wilderness Survival',
           grade: 66,
         },
-      ].map(data => Test.create(data));
-      return Promise.all(creatingTests).then(createdTests => {
-        funTest = createdTests[0];
-        badTest = createdTests[1];
-        hardTest = createdTests[2];
-        crayTest = createdTests[3];
-      });
-    });
+      ].map((data) => Test.create(data))
+      return Promise.all(creatingTests).then((createdTests) => {
+        funTest = createdTests[0]
+        badTest = createdTests[1]
+        hardTest = createdTests[2]
+        crayTest = createdTests[3]
+      })
+    })
     afterEach(() => {
       return Promise.all([
         Student.truncate({ cascade: true }),
         Test.truncate({ cascade: true }),
-      ]);
-    });
+      ])
+    })
 
     describe('GET /tests', () => {
       it('retrieves all tests', () => {
         return agent
           .get('/tests')
           .expect(200)
-          .expect(res => {
-            expect(res.body).to.be.an.instanceOf(Array);
-            expect(res.body).to.have.length(4);
-          });
-      });
-    });
+          .expect((res) => {
+            expect(res.body).to.be.an.instanceOf(Array)
+            expect(res.body).to.have.length(4)
+          })
+      })
+    })
 
     describe('GET /tests/:id', () => {
       it('gets the test instance by id', () => {
         return agent
           .get(`/tests/${funTest.id}`)
           .expect(200)
-          .expect(res => {
-            expect(res.body.subject).to.equal(funTest.subject);
-          });
-      });
-    });
+          .expect((res) => {
+            expect(res.body.subject).to.equal(funTest.subject)
+          })
+      })
+    })
 
     describe('POST /tests/student/:studentId', () => {
-      let student;
+      let student
       beforeEach(() => {
         return Student.create({
           firstName: 'Pepper',
           lastName: 'Potts',
           email: 'saltn@pepper.com',
-        }).then(newStudent => {
-          student = newStudent;
-        });
-      });
+        }).then((newStudent) => {
+          student = newStudent
+        })
+      })
       it('creates a new Test instance for a student', () => {
         return agent
           .post(`/tests/student/${student.id}`)
@@ -205,22 +205,22 @@ describe('Routes', () => {
           })
           .expect(201)
           .expect('Content-Type', /json/)
-          .expect(res => {
-            expect(res.body.studentId).to.equal(student.id);
-          });
-      });
-    });
+          .expect((res) => {
+            expect(res.body.studentId).to.equal(student.id)
+          })
+      })
+    })
     describe('DELETE /tests/:id', () => {
       it('deletes an instance of test by its id', () => {
         return agent
           .delete(`/tests/${crayTest.id}`)
           .expect(204)
           .expect(() => {
-            return Test.findById(crayTest.id).then(res => {
-              expect(res).to.equal(null);
-            });
-          });
-      });
-    });
-  });
-});
+            return Test.findByPk(crayTest.id).then((res) => {
+              expect(res).to.equal(null)
+            })
+          })
+      })
+    })
+  })
+})
