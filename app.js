433316e8bf62f6b1a20855289f5f031685089c05
Mary Gordanier <mcgordanier@gmail.com>
2020-11-15 18:29:40 -0500

    refactor: change .then to async/await for app init

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const db = require('./db/db');
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const morgan = require('morgan')
+const db = require('./db/db')
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(morgan('dev'));
+app.use(morgan('dev'))
 
-app.use(function(err, req, res, next) {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
-if (require.main === module) {
-  //will only run when run with npm start and not with npm test to avoid db syncing in multiple threads when running tests
-  db.sync()
-    .then(() =>
-      app.listen(3000, function() {
-        console.log('Server is listening on port 3000!');
+const init = async () => {
+  if (require.main === module) {
+    //will only run when run with npm start and not with npm test to avoid db syncing in multiple threads when running tests
+    try {
+      await db.sync()
+      app.listen(3000, () => {
+        console.log('Server is listening on port 3000!')
       })
-    )
-    .catch(console.error);
+    } catch (err) {
+      console.error(err)
+    }
+  }
 }
 
-module.exports = app;
+init()
+
+module.exports = app
